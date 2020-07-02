ea36d3e9c43f6ffb6b621cc2b03fcd11640c4448
jessica <jessica.farias.rosado@gmail.com>
2020-07-01 23:23:08 -0500

    Adding a basic contact form

--- Conecction/FrontPage.php
+++ Conecction/FrontPage.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php 
-            $nombre= $_POST["nombre"];
-            echo "SU NOMBRE nuevo es ES"+ $nombre;
-            include 'DbConnection.php';
-            include 'DbFactory.php';
-            include 'DbConnectionData.php';
-            use MyConnection\DbConnectionData as conn;
-            conn::update(1,$nombre);
-        ?>
-    </body>
-
-</html>
+<?php 
+    $nombre= $_POST["nombre"];
+    echo "SU NOMBRE nuevo es ES"+ $nombre;
+    include 'DbConnection.php';
+    include 'DbFactory.php';
+    include 'DbConnectionData.php';
+    use MyConnection\DbConnectionData as conn;
+    conn::update(1,$nombre);
+?>
---
+++ Conecction/Sent.php
@@ -0,0 +1,21 @@
+<?php
+if (!$_POST){}
+
+else{
+	 
+    $Email = "support@jessicafarias.dx.am";
+    $recipientEmail = $_POST["email"];
+
+    $from = "Jessica Farias support <" . $Email . ">";
+    $to = $recipientEmail;
+
+    $subject = "Consulta";
+    $body = $_POST["consulta"];
+    if(mail($to,$subject,$body,$from)){
+        echo 'E-mail message sent!';
+        include 'success.html'; 
+    } else {
+        echo 'E-mail delivery failure!';
+    }
+}
+?>
